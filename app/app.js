386b30ab6ba5a70df38762bbd4d92ba3d9b311b8
ccjmne <ccjmne@gmail.com>
2016-02-01 22:46:46 +0100

    Visualising some sample site statistics from FAOMaintenance.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,227 @@
 'use strict';
 
-document.getElementById('world').style['text-decoration'] = 'underline';
+var d3 = window.d3 = require('d3');
+var _ = require('lodash');
+
+var locale = require('./resources/fr-FR.js');
+
+var margin = {
+		top: 20,
+		right: 20,
+		bottom: 30,
+		left: 50
+	},
+  width = 500 - margin.left - margin.right,
+  height = 150 - margin.top - margin.bottom;
+
+var svg = d3.select('body').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+	.append('g')
+  	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+
+var focusTimeFormat = locale.timeFormat('%b %Y'),
+	axisTimeFormat = locale.timeFormat.multi([
+	  ['.%L', function(d) { return d.getMilliseconds(); }],
+	  [':%S', function(d) { return d.getSeconds(); }],
+	  ['%I:%M', function(d) { return d.getMinutes(); }],
+	  ['%I %p', function(d) { return d.getHours(); }],
+	  ['%a %d', function(d) { return d.getDay() && d.getDate() !== 1; }],
+	  ['%b %d', function(d) { return d.getDate() !== 1; }],
+	  ['%B', function(d) { return d.getMonth(); }],
+	  ['%Y', function() { return true; }]
+	]);
+
+
+var dateBisector = d3.bisector(function (d) {
+	return d.date;
+}).left;
+
+function getClosestEntry (data, scale, pos) {
+	var date = scale.invert(pos),
+		idx = dateBisector(data, date),
+		hi = data[idx],
+		lo = data[idx -1];
+
+	if (hi === undefined) {
+		return lo;
+	}
+
+	if (lo === undefined) {
+		return hi;
+	}
+
+	return date - lo.date > hi.date - date ? hi : lo;
+}
+
+d3
+	.json('./resources/data.json', function (error, data) {
+		data = _(data).map(function (entry, date) {
+			return {
+				count: entry.certificates['1'].count,
+				target: entry.certificates['1'].target,
+				date: new Date(date)
+			};
+		}).sortBy('date').value();
+
+		var x = d3.time.scale().domain([_.minBy(data, 'date').date, _.maxBy(data, 'date').date]).range([0, width]).nice();
+		var y = d3.scale.linear().domain([0, _.reduce(data, function (max, entry) {
+			return _.max([max, entry.count, entry.target]);
+		}, 0)]).range([height, 0]).nice();
+
+		var xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(width / 100);
+		var yAxis = d3.svg.axis().scale(y).orient('left').ticks(height / 30);
+
+
+// GRID
+		svg.append('g')
+      .attr('class', 'grid')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis
+        .tickSize(-height, 0)
+        .tickFormat('')
+      );
+    svg.append('g')         
+      .attr('class', 'grid')
+      .call(yAxis
+        .tickSize(-width, 0)
+        .tickFormat('')
+      );
+
+
+// AXES
+		svg.append('g')
+			.attr('transform', 'translate(0,' + height+ ')')
+			.attr('class', 'x axis')
+			.call(xAxis
+				.tickSize(6, 0)
+				.tickFormat(axisTimeFormat)
+			);
+		svg.append('g')
+			.attr('class', 'y axis')
+			.call(
+				yAxis.tickSize(6, 0)
+				.tickFormat(d3.format('d'))
+			);
+
+
+// ---------------------------------------------------------
+// LINES AND AREAS DEFINITION
+// ---------------------------------------------------------
+
+		var countLine = d3.svg.line()
+	    .interpolate('monotone')
+	    .x(function(d) {
+	    	return x(d.date);
+	    })
+	    .y(function(d) {
+	    	return y(d.count);
+	    });
+		var targetLine = d3.svg.line()
+	    .interpolate('monotone')
+	    .x(countLine.x())
+	    .y(function(d) {
+	    	return y(d.target);
+	    });
+
+// CLIPPING AREAS
+		var defs = svg.append('defs');
+		defs.append('clipPath')
+		  .attr('id', 'clip-count')
+		  .append('path')
+			  .datum(data)
+			  .attr('d', d3.svg.area()
+			    .interpolate('monotone')
+				  .x(countLine.x())
+				  .y0(countLine.y())
+				  .y1(0));
+		defs.append('clipPath')
+		  .attr('id', 'clip-target')
+		  .append('path')
+			  .datum(data)
+			  .attr('d', d3.svg.area()
+			    .interpolate('monotone')
+				  .x(targetLine.x())
+				  .y0(targetLine.y())
+				  .y1(0));
+
+// LINES
+	  svg.append('path')
+      .attr('class', 'count line')
+      .datum(data)
+      .attr('d', countLine);
+	  svg.append('path')
+      .attr('class', 'target line')
+      .datum(data)
+      .attr('d', targetLine);
+
+// AREAS
+		svg.append('path')
+		  .datum(data)
+		  .attr('class', 'count area')
+		  .attr('clip-path', 'url(#clip-target)')
+		  .attr('d', d3.svg.area()
+		    .interpolate('monotone')
+			  .x(countLine.x())
+			  .y0(countLine.y())
+			  .y1(height));
+		svg.append('path')
+		  .datum(data)
+		  .attr('class', 'target area')
+		  .attr('clip-path', 'url(#clip-count)')
+		  .attr('d', d3.svg.area()
+		    .interpolate('monotone')
+			  .x(targetLine.x())
+			  .y0(targetLine.y())
+			  .y1(height));
+
+
+// ---------------------------------------------------------
+// FOCUS DEFINITION
+// ---------------------------------------------------------
+
+		var focus = svg.append('g')
+    	.style('display', 'none');
+
+		focus.append('circle') 
+      .attr('class', 'count line')
+      .attr('r', 4);
+
+		var text = focus.append('text')
+				.attr('class', 'tooltip')
+				.attr('text-anchor', 'middle')
+				.attr('transform', 'translate(0, -5)'),
+			count = focus.append('text')
+				.attr('class', 'tooltip')
+				.attr('text-anchor', 'middle')
+				.attr('transform', 'translate(0, 15)'),
+			target = focus.append('text')
+				.attr('class', 'tooltip')
+				.attr('text-anchor', 'middle')
+				.attr('transform', 'translate(0, 25)');
+
+
+// ---------------------------------------------------------
+// HOVERING ZONE
+// ---------------------------------------------------------
+
+		svg.append('rect')
+      .attr('width', width)
+      .attr('height', height)
+      .style('fill', 'none')
+      .style('pointer-events', 'all')
+      .on('mouseover', function () {
+      	focus.style('display', null);
+      })
+      .on('mouseout', function () {
+      	focus.style('display', 'none');
+      })
+      .on('mousemove', function () {
+      	var d = getClosestEntry(data, x, d3.mouse(this)[0]);
+			  focus.attr('transform', 'translate(' + x(d.date) + ',' +  y(d.count) + ')');
+			  text.text(focusTimeFormat(d.date));
+			  count.text('SST : ' + d.count);
+			  target.text('Cible : ' + d.target);
+			});
+	});
