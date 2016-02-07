529335c9901cbfa9e9fb4f2697f910f800761786
ccjmne <ccjmne@gmail.com>
2016-02-08 00:43:45 +0100

    Added legend. Now choosing which certificate's statistics to display according to some html select's value.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 'use strict';
+/* jshint camelcase: false */
 
 require('./scripts/employeeStats.js');
 
@@ -8,14 +9,14 @@ var _ = require('lodash');
 var locale = require('./resources/fr-FR.js');
 var focusTimeFormat = locale.timeFormat('%d %b %Y'),
 	axisTimeFormat = locale.timeFormat.multi([
-	  ['.%L', function(d) { return d.getMilliseconds(); }],
-	  [':%S', function(d) { return d.getSeconds(); }],
-	  ['%I:%M', function(d) { return d.getMinutes(); }],
-	  ['%I %p', function(d) { return d.getHours(); }],
-	  ['%a %d', function(d) { return d.getDay() && d.getDate() !== 1; }],
-	  ['%b %d', function(d) { return d.getDate() !== 1; }],
-	  ['%B', function(d) { return d.getMonth(); }],
-	  ['%Y', function() { return true; }]
+	  ['.%L', function (d) { return d.getMilliseconds(); }],
+	  [':%S', function (d) { return d.getSeconds(); }],
+	  ['%I:%M', function (d) { return d.getMinutes(); }],
+	  ['%I %p', function (d) { return d.getHours(); }],
+	  ['%a %d', function (d) { return d.getDay() && d.getDate() !== 1; }],
+	  ['%b %d', function (d) { return d.getDate() !== 1; }],
+	  ['%B', function (d) { return d.getMonth(); }],
+	  ['%Y', function () { return true; }]
 	]);
 
 var dateBisector = d3.bisector(function (d) {
@@ -39,7 +40,18 @@ function getClosestEntry (data, scale, pos) {
 	return date - lo.date > hi.date - date ? hi : lo;
 }
 
-var certData;
+var cert, certData;
+
+var certificates;
+d3.json('./resources/certificates.json', function (error, data) {
+	certificates = data;
+
+	var select = d3.select('body').append('select');
+	select.selectAll('option').data(_.values(certificates)).enter().append('option').attr('value', function (d) { return d.cert_pk; }).text(function (d) { return d.cert_short; });
+	select.node().addEventListener('change', function () {
+		displayData(this.value);
+	}, false);
+});
 
 
 // ---------------------------------------------------------
@@ -48,11 +60,11 @@ var certData;
 
 var margin = {
 		top: 20,
-		right: 20,
+		right: 100,
 		bottom: 30,
 		left: 50
 	},
-  width = 500 - margin.left - margin.right,
+  width = 580 - margin.left - margin.right,
   height = 150 - margin.top - margin.bottom;
 
 var svg = d3.select('body').append('svg')
@@ -92,12 +104,12 @@ svg.append('g')
 
 var countLine = d3.svg.line()
   .interpolate('monotone')
-  .x(function(d) {return x(d.date);})
-  .y(function(d) {return y(d.count);});
+  .x(function (d) { return x(d.date); })
+  .y(function (d) { return y(d.count); });
 var targetLine = d3.svg.line()
   .interpolate('monotone')
   .x(countLine.x())
-  .y(function(d) {return y(d.target);});
+  .y(function (d) { return y(d.target); });
 
 var areaAboveCount = d3.svg.area()
   .interpolate('monotone')
@@ -181,7 +193,7 @@ highlightTarget.append('circle')
   .attr('class', 'target line')
   .attr('r', 4);
 highlightTarget.append('text')
-	.attr('class', 'tooltip')
+	.attr('class', 'target')
 	.attr('text-anchor', 'middle');
 
 var highlightCount = highlight.append('g');
@@ -189,8 +201,16 @@ highlightCount.append('circle')
   .attr('class', 'count line')
   .attr('r', 4);
 highlightCount.append('text')
-	.attr('class', 'tooltip')
+	.attr('class', 'count')
 	.attr('text-anchor', 'middle');
+
+var legend = svg.append('g').attr('transform', 'translate(' + width + ', ' + height/2 + ')').style('fill', 'black');
+legend.append('path').attr('d', 'M10,-10h40').attr('class', 'line count');
+//legend.append('circle').attr('r', 4).attr('cx', 30).attr('cy', -10).attr('class', 'line count');
+var certLegend = legend.append('text').attr('class', 'count').attr('transform', 'translate(55, -5)');
+legend.append('path').attr('d', 'M10,10h40').attr('class', 'line target').style('shape-rendering', 'crispEdges');
+//legend.append('circle').attr('r', 4).attr('cx', 30).attr('cy', 10).attr('class', 'line target');
+legend.append('text').attr('class', 'target').attr('transform', 'translate(55, 15)').text('Cible');
 
 
 
@@ -219,7 +239,7 @@ svg.append('rect')
 	  highlightCount.select('text')
 	  	.transition().duration(100).ease(d3.ease('linear'))
 	  	.attr('transform', 'translate(0, ' + (d.target > d.count ? 17 : -8) + ')')
-	  	.text('SST : ' + d.count);
+	  	.text(cert.cert_short + ' : ' + d.count);
 
 	  highlightTarget.transition().duration(100).ease(d3.ease('linear')).attr('transform', 'translate(' + x(d.date) + ',' +  y(d.target) + ')');
 	  highlightTarget.select('text')
@@ -236,12 +256,13 @@ svg.append('rect')
 
 
 
-function displayData (cert) {
+function displayData (cert_pk) {
 	d3.json('./resources/2399.json', function (error, data) {
+		cert = certificates[cert_pk];
 		certData = _(data).map(function (entry, date) {
 			return {
-				count: entry.certificates[cert].count,
-				target: entry.certificates[cert].target,
+				count: entry.certificates[cert_pk].count,
+				target: entry.certificates[cert_pk].target,
 				date: new Date(date)
 			};
 		}).sortBy('date').value();
@@ -267,9 +288,9 @@ function displayData (cert) {
 
 	  svg.select('.target.line').transition().duration(1000).attr('d', targetLine(certData));
 	  svg.select('.count.line').transition().duration(1000).attr('d', countLine(certData));
+
+	  certLegend.text(cert.cert_short);
 	});
 }
 
 displayData('1');
-setTimeout(function () {displayData('4');}, 1000);
-setTimeout(function () {displayData('1');}, 3000);
